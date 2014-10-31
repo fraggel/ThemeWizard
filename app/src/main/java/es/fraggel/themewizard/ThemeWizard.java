44dfdd83beedbf7e19f73cd663621a4d6ca00c9b
fraggel <fraggelillo666@gmail.com>
2014-10-31 18:12:35 +0100

    version 2

--- app/src/main/java/es/fraggel/themewizard/ThemeWizard.java
+++ app/src/main/java/es/fraggel/themewizard/ThemeWizard.java
@@ -78,15 +78,35 @@ public class ThemeWizard extends Activity {
             // Return a PlaceholderFragment (defined as a static inner class below).
             switch (position){
                 case 1:
-                    return PlaceholderFragment1.newInstance(position);
-                case 2:
                     return PlaceholderFragment2.newInstance(position);
+                case 2:
+                    return PlaceholderFragment3.newInstance(position);
                 case 3:
-                    return new PlaceholderFragment3().newInstance(position);
-                case 4:
                     return PlaceholderFragment4.newInstance(position);
-                default:
+                case 4:
                     return PlaceholderFragment5.newInstance(position);
+                case 5:
+                    return PlaceholderFragment6.newInstance(position);
+                case 6:
+                    return PlaceholderFragment7.newInstance(position);
+                case 7:
+                    return PlaceholderFragment8.newInstance(position);
+                case 8:
+                    return PlaceholderFragment9.newInstance(position);
+                case 9:
+                    return PlaceholderFragment10.newInstance(position);
+                case 10:
+                    return PlaceholderFragment11.newInstance(position);
+                case 11:
+                    return PlaceholderFragment12.newInstance(position);
+                case 12:
+                    return PlaceholderFragment13.newInstance(position);
+                case 13:
+                    return PlaceholderFragment14.newInstance(position);
+                case 14:
+                    return new PlaceholderFragment15().newInstance(position);
+                default:
+                    return PlaceholderFragment1.newInstance(position);
             }
 
         }
@@ -94,7 +114,7 @@ public class ThemeWizard extends Activity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 4;
+            return 16;
         }
 
         @Override
@@ -108,6 +128,28 @@ public class ThemeWizard extends Activity {
                 case 2:
                     return getString(R.string.title_section3).toUpperCase(l);
                 case 3:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 4:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 5:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 6:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 7:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 8:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 9:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 10:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 11:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 12:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 13:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 14:
                     return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
@@ -143,7 +185,7 @@ public class ThemeWizard extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.fragment_theme_wizard2, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard1, container, false);
             return rootView;
         }
     }
@@ -173,22 +215,22 @@ public class ThemeWizard extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.fragment_theme_wizard3, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard2, container, false);
             return rootView;
         }
     }
-    public class PlaceholderFragment3 extends Fragment implements View.OnClickListener {
+    public static class PlaceholderFragment3 extends Fragment{
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
-        private final String ARG_SECTION_NUMBER = "section_number";
+        private static final String ARG_SECTION_NUMBER = "section_number";
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public PlaceholderFragment3 newInstance(int sectionNumber) {
+        public static PlaceholderFragment3 newInstance(int sectionNumber) {
             PlaceholderFragment3 fragment = new PlaceholderFragment3();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
@@ -203,19 +245,10 @@ public class ThemeWizard extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.fragment_theme_wizard4, container, false);
-            Button button = (Button) rootView.findViewById(R.id.button);
-            button.setOnClickListener(this);
-
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard3, container, false);
             return rootView;
         }
 
-        @Override
-        public void onClick(View view) {
-            if(view.getId()==R.id.button){
-                finish();
-            }
-        }
     }
     public static class PlaceholderFragment4 extends Fragment{
         /**
@@ -274,9 +307,326 @@ public class ThemeWizard extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.fragment_theme_wizard1, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard5, container, false);
             return rootView;
         }
     }
+    public static class PlaceholderFragment6 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment6 newInstance(int sectionNumber) {
+            PlaceholderFragment6 fragment = new PlaceholderFragment6();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment6() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard6, container, false);
+            return rootView;
+        }
+    }
+    public static class PlaceholderFragment7 extends Fragment{
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment7 newInstance(int sectionNumber) {
+            PlaceholderFragment7 fragment = new PlaceholderFragment7();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment7() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard7, container, false);
+            return rootView;
+        }
+
+    }
+    public static class PlaceholderFragment8 extends Fragment{
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment8 newInstance(int sectionNumber) {
+            PlaceholderFragment8 fragment = new PlaceholderFragment8();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment8() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard8, container, false);
+            return rootView;
+        }
+
+    }
+    public static class PlaceholderFragment9 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment9 newInstance(int sectionNumber) {
+            PlaceholderFragment9 fragment = new PlaceholderFragment9();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment9() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard9, container, false);
+            return rootView;
+        }
+    }
+    public static class PlaceholderFragment10 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment10 newInstance(int sectionNumber) {
+            PlaceholderFragment10 fragment = new PlaceholderFragment10();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment10() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard10, container, false);
+            return rootView;
+        }
+    }
+    public static class PlaceholderFragment11 extends Fragment{
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment11 newInstance(int sectionNumber) {
+            PlaceholderFragment11 fragment = new PlaceholderFragment11();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment11() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard11, container, false);
+            return rootView;
+        }
+
+    }
+    public static class PlaceholderFragment12 extends Fragment{
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment12 newInstance(int sectionNumber) {
+            PlaceholderFragment12 fragment = new PlaceholderFragment12();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment12() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard12, container, false);
+            return rootView;
+        }
+
+    }
+
+    public static class PlaceholderFragment13 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment13 newInstance(int sectionNumber) {
+            PlaceholderFragment13 fragment = new PlaceholderFragment13();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment13() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard13, container, false);
+            return rootView;
+        }
+    }
+    public static class PlaceholderFragment14 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment14 newInstance(int sectionNumber) {
+            PlaceholderFragment14 fragment = new PlaceholderFragment14();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment14() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard14, container, false);
+            return rootView;
+        }
+    }
+    public static class PlaceholderFragment15 extends Fragment implements View.OnClickListener {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public PlaceholderFragment15 newInstance(int sectionNumber) {
+            PlaceholderFragment15 fragment = new PlaceholderFragment15();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment15() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_theme_wizard15, container, false);
+            Button button = (Button) rootView.findViewById(R.id.button);
+            button.setOnClickListener(this);
+
+            return rootView;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(view.getId()==R.id.button){
+                getActivity().finish();
+            }
+        }
+    }
+
+
 
 }
